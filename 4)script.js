33e2b352387cf41a3925e86e9affdaf8baca3825
Giulio_Pa <giuliop93@hotmail.it>
2021-04-23 16:23:14 +0200

    help

--- 4)script.js
+++ 4)script.js
@@ -1,7 +1,67 @@
-[snack 1]
 crea 10 oggetti che rappresentano una zucchina, indicandone per ognuno varietà, peso e lunghezza. calcola quanto pesano tutte le zucchine
+var cassetta []
 
-[snack 2]
-crea 10 oggetti che rappresentano una zucchina.
-dividi in due array separati le zucchine che misurano meno o più di 15 cm.
+var zucchinaUno = {
+    variety: "nero di Milano",
+    weight: null,
+    length: 10
+}
+var zucchinaDue = {
+    variety: "zucchino romanesco",
+    weight: null,
+    length: 11
+}
+var zucchinaTre = {
+    variety: "ortolano di Faenza",
+    weight: null,
+    length: 12
+}
+var zucchinaQuattro = {
+    variety: "lunga fiorentina",
+    weight: null,
+    length: 13
+}
+var zucchinaCinque = {
+    variety: "zucchino siciliano",
+    weight: null,
+    length: 14
+}
+var zucchinaSei = {
+    variety: "striata di Napoli",
+    weight: null,
+    length: 15
+}
+var zucchinaSette = {
+    variety: "bianca triestina",
+    weight: null,
+    length: 16
+}
+var zucchinaOtto = {
+    variety: "rigata pugliese",
+    weight: null,
+    length: 17
+}
+var zucchinaNove = {
+    variety: "tondo di Piacenza",
+    weight: null
+    length: 18
+}
+var zucchinaDieci = {
+    variety: "tondo di Nizza",
+    weight: null
+    length: 19
+}
+
+  
+//dividi in due array separati le zucchine che misurano meno o più di 15 cm.
+
+var shortCourgettes = []
+var longCourgettes = []
+
+for (i = 0; i < 10; i++){
+    if 
+}
+
+for (var key in )
+
 infine stampa separatamente quanto pesano i due gruppi di zucchine.
